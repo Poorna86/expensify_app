17ea8abad00ee029769da9c82155dc554f61010a
Poorna <bobachandra@gmail.com>
2020-11-09 23:04:42 +0530

    setup google sign and connection with firebase

--- server/server.js
+++ server/server.js
@@ -12,24 +12,24 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(publicPath, 'index.html'))
 })
 
-app.post('/create', async (req,res) => {
-    require('dotenv').config({ path: '.env.example' })
-    const phno = '+916363027399'
-    const client = require('twilio')(
-        process.env.TWILIO_ACCOUNT_SID,
-        process.env.TWILIO_AUTH_TOKEN
-    );
+// app.post('/create', async (req,res) => {
+//     require('dotenv').config({ path: '.env.example' })
+//     const phno = '+916363027399'
+//     const client = require('twilio')(
+//         process.env.TWILIO_ACCOUNT_SID,
+//         process.env.TWILIO_AUTH_TOKEN
+//     );
 
-    client.messages.create({
-        from: process.env.TWILIO_NUMBER,
-        to: phno,
-        body: "You just sent an SMS from Node.js using Twilio!"
-    }).then((messsage) => {
-        console.log(messsage.sid)})
-      .catch((e) => {
-        console.log('error: ', e)
-    });
-})
+//     client.messages.create({
+//         from: process.env.TWILIO_NUMBER,
+//         to: phno,
+//         body: "You just sent an SMS from Node.js using Twilio!"
+//     }).then((messsage) => {
+//         console.log(messsage.sid)})
+//       .catch((e) => {
+//         console.log('error: ', e)
+//     });
+// })
 
 app.listen(port, () => {
     console.log('server up running')
